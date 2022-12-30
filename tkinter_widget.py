bb00b2d3159599db68235ff6235233a07a79ccee
Rannael Santiago <74615002+rannaelsant@users.noreply.github.com>
2022-12-30 11:22:17 -0300

    Update tkinter_widget.py

--- tkinter_widget.py
+++ tkinter_widget.py
@@ -21,10 +21,13 @@ entry.insert(END, string="Some text to begin with.")
 print(entry.get())
 entry.pack()
 
+#Text
 text = Text(height=5, width=30)
+# Puts cursor in textbox.
 text.focus()
+# Add some text to begin with.
 text.insert(END, "Example of multi-line text entry.")
-
+# Gets current value in textbox at line 1, charecter 0
 print(text.get("1.0", END))
 text.pack()
 
@@ -39,6 +42,8 @@ spinbox.pack()
 
 def scale_used(value):
     print(value)
+
+
 scale = Scale(from_=0, to=100,
 command=scale_used)
 scale.pack()
@@ -71,5 +76,20 @@ radiobutton2 = Radiobutton(text="Option_2",
                            command=radio_used)
 radiobutton1.pack()
 radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(list.curselection()))
+
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Orange", "Grape", "Strawberry"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>",
+             listbox_used())
+listbox.pack()
 
 window.mainloop()
